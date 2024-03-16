502f4d677dc7e7ccce808711e712d17e44246c32
Shirley_Grace <giray.mungai@gmail.com>
2024-03-16 14:54:03 +0300

    area

--- areaperimeter.js
+++ areaperimeter.js
@@ -1,20 +1,19 @@
 
 //AREA
 
-/*let length = 10 ;
+let length = 10 ;
 let width = 5;
  let area = (length * width);
 
  console.log(area);
 
 
- */
 
 
  //PERIMETER
-let length = 15;
-let width = 10;
-  let perimeter = ((2*length) +(2*width));
+let length1 = 15;
+let width1 = 10;
+  let perimeter = ((2*length1) +(2*width1));
 
 console.log(perimeter);
 
